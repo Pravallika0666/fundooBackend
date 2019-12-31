bfef7a96163ca11bb4b5a66b010cdb1678247f96
Pravallika <skpravallikachowdary2@gmail.com>
2019-12-31 14:58:10 +0530

    [Pravallika]refactor.modified user controller of the fundoo for better readability and pushed into remote

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -45,3 +45,39 @@ exports.register = (request, res) => {
  * @param callback sends the data back or err
  * @return responses with a http response
 ***********************************************************/
+exports.login = (request, res) => {
+    console.log("Login")
+    request.checkBody('email', 'Email is invalid').isEmail().notEmpty();
+    request.checkBody('password', 'Password is Invalid').notEmpty().len(7, 13);
+    var error = request.validationErrors();
+    var response = {};
+    if (error) {
+        response.error = error;
+        response.success = false;
+        res.status(422).send(response)
+    } else {
+        Services.login(request, (err, data) => {
+            if (err) {
+                response.success = false;
+                response.data = err;
+
+                res.status(404).send(response);
+            } else {
+                response.success = true;
+                let data1 = []
+                let newToken = tokenGenerate.generateToken({
+                    "email": request.body.email,
+                    "id": data._id
+                })
+                console.log("NEW TOKEN", newToken);
+
+                let value = newToken.token;
+                data1.push(newToken);
+                data1.push(data)
+                console.log("Generated token with paste login data--->", data1);
+                response.data = data1
+                res.status(200).send(response);
+            }
+        })
+    }
+}
