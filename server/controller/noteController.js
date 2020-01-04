59e14535533a123bf3b1902e51a75da4e6399fd8
Pravallika <skpravallikachowdary2@gmail.com>
2020-01-04 12:19:40 +0530

    [Pravallika]refactor.modified note controller of the fundoo for better readability and pushed into remote

--- server/controller/noteController.js
+++ server/controller/noteController.js
@@ -35,7 +35,12 @@ exports.addNote=(request,res)=>{
         
     }
 }
-
+/**********************************************************
+ *  @desc Gets the input from front end pass to model
+ *  @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
 exports.getAllnote=(request,res)=>{
     try{
         let response={}
