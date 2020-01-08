8959909d291bfe4b5b8fcb3d4af62c4e5a052c38
Pravallika <skpravallikachowdary2@gmail.com>
2020-01-08 15:08:25 +0530

    [Pravallika]refactor.modified note controller of the fundoo backend for better readability and pushed into remote

--- server/controller/noteController.js
+++ server/controller/noteController.js
@@ -94,3 +94,28 @@ exports.deleteNote = (request, res) => {
         console.log(e)
     }
 }
+/**********************************************************
+ *  @desc Gets the input from front end pass to model
+ *  @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+//exports update note
+exports.updateNote=(request,res)=>{
+    try{
+        let response={}
+        noteServices.updateNote(request)
+        .then((data)=>{
+            response.status=true;
+            response.data=data
+            res.status(200).send(response)
+        })
+        .catch((err)=>{
+            response.success=false;
+            response.err=err
+            res.status(404).send(response)
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
