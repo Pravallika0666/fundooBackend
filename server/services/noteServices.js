63b1b109879fde878300938cae4052b46b64c2c0
Pravallika <skpravallikachowdary2@gmail.com>
2020-01-08 14:58:09 +0530

    [Pravallika]refactor.modified note services of the fundoo backend for better readability and pushed into remote

--- server/services/noteServices.js
+++ server/services/noteServices.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 const noteModel = require('../model/noteModel')
-
+/**********************************************************
+ *  @desc Gets the input from front end pass to model
+ *  @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
 exports.addNote = (request) => {
     try {
         return new Promise((resolve, reject) => {
@@ -21,23 +26,51 @@ exports.addNote = (request) => {
         console.log(e)
     }
 }
-exports.getAllnote=(request)=>{
-    try{
-        return new Promise((resolve,reject)=>{
-            let noteDetails=new noteModel.notes({
-                "userId":request.decoded.payload.id,
-                "title":request.body.title,
-                "description":request.body.description
+/**********************************************************
+ *  @desc Gets the input from front end pass to model
+ *  @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+exports.getAllnote = (request) => {
+    try {
+        return new Promise((resolve, reject) => {
+            let noteDetails = new noteModel.notes({
+                "userId": request.decoded.payload.id,
+                "title": request.body.title,
+                "description": request.body.description
             })
-            noteDetails.save((err,data)=>{
-                if(data){
+            noteDetails.save((err, data) => {
+                if (data) {
                     resolve(data)
-                }else{
+                } else {
                     reject(err)
                 }
             })
         })
-    }catch(e){
+    } catch (e) {
+        console.log(e)
+    }
+}
+/**********************************************************
+ *  @desc Gets the input from front end pass to model
+ *  @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+exports.deleteNote = (request) => {
+    try {
+        return new Promise((resolve, reject) => {
+            noteModel.notes.findByIdAndUpdate({ _id: req.body.userId }, { isDeleted: true }, (err, result) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(result)
+                }
+            })
+        })
+    }
+    catch (e) {
         console.log(e)
     }
 }
