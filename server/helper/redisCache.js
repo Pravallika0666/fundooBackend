0449de7774061fd977558f8488df60d0fd09982c
Pravallika <skpravallikachowdary2@gmail.com>
2020-01-10 18:43:17 +0530

    [Pravallika]refactor.modified redis cache for better readability and pushed into remote

--- server/helper/redisCache.js
+++ server/helper/redisCache.js
@@ -7,6 +7,62 @@ client.on('connect', function () {
 client.on('error', (err) => {
     console.log('Error' + err)
 })
+
+/**
+ * Below three function used to store the token to verify
+ **/
+
+exports.setRedis = (val, callback) => {
+    console.log("Vlaue------>",val);
+    
+
+    client.set(process.env.key, JSON.stringify(val), (err, result) => {
+        if (result) {
+            console.log("token set in cache", result);
+            callback(null, result);
+
+        } else {
+            console.log("error in setting data");
+            callback(err);
+        }
+    })
+}
+
+exports.getRedis = (callback) => {
+    client.get(process.env.key, (err, data) => {
+        if (data) {
+            callback(null, data);
+            // console.log("GET DATA-->");
+        } else {
+            callback(err);
+            console.log("no data");
+
+        }
+    })
+}
+delRedis = () => {
+    client.del(process.env.key, (err, data) => {
+        if (data) {
+            console.log("Data to delete", data);
+        } else {
+            console.log("no data");
+
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 //add cache
 exports.addRedisCache = (data, callback) => {
     client.set(process.env.CACHEKEY, JSON.stringify(data), (err, data) => {
@@ -31,7 +87,7 @@ exports.getRedisCache = (callback) => {
 //set redis note is used to set the note 
 //exports set redis note to the cache
 exports.setRedisNote = (data, callback) => {
-    client.set(process.env.CACHEKEY + data.id, JSON.stringify(data.result), (err, data) => {
+    client.set(process.env.CACHEKEY + data.id, JSON.stringify(data.notes), (err, data) => {
         if (err) {
             callback(err);
             console.log("error");
@@ -45,8 +101,12 @@ exports.setRedisNote = (data, callback) => {
 //get redis note is used to get the data from the cache 
 //exports get redis note 
 exports.getRedisNote = (id, callback) => {
+    console.log("HJIUY",process.env.CACHEKEY);
+    
     client.get(process.env.CACHEKEY + id, (err, data) => {
         if (data) {
+            console.log('ldjd',data);
+            
             let data1 = JSON.parse(data)
             callback(null, data1)
             console.log('cached data')
@@ -63,7 +123,7 @@ exports.deleteRedisNote = (id, callback) => {
     client.del(process.env.CACHEKEY + id, (err, data) => {
         if (data) {
             callback(data)
-            console.log("data",data)
+            console.log("data deleted in cahce",data)
         } else {
             callback(err)
             console.log("error")
