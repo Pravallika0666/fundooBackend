50d9a3050cd896444f8171a0a8e6b2933c5c85db
Pravallika <skpravallikachowdary2@gmail.com>
2020-01-16 16:24:27 +0530

    [Pravallika]refactor.modified user router for better readability and pushed into remote

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -2,9 +2,12 @@ const express=require('express')
 const router=express.Router()
 const userController=require('../controller/userController')
 const token=require('../middlewares/token')
+const userToken=require('../middlewares/token')
+const upload=require('../helper/multer')
 
 router.post('/register',userController.register)//router for the register
 router.post('/login',userController.login)//router for the login
 router.post('/forgotpassword',userController.forgotpassword)//router for the forgotpassword
 router.post('/resetpassword/:token',token.verify,userController.resetpassword)//router for resetpassword
+router.post('/imageUpload',userToken.userVerify,upload.single('image'),userController.imageUpload)
 module.exports=router;
