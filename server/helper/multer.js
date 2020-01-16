5a8a2cef9d6f7f128391f48b446d1c453d0827ef
Pravallika <skpravallikachowdary2@gmail.com>
2020-01-16 16:25:52 +0530

    [Pravallika]refactor.modified multer for better readability and pushed into remote

--- server/helper/multer.js
+++ server/helper/multer.js
@@ -1,22 +1,50 @@
-require('dotenv').config()
-const multer = require('multer')
-const multers3 = require('multer-s3')
-const aws = require('aws-sdk')
+require('dotenv').config();
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
 
-const accesskeyId = process.env.ACCESS_KEY_ID
-const successkeyId = process.env.SUCCESS_ACCESS_KEY
-const bucketName = process.env.BUCKET_NAME
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const Bucket = process.env.BUCKET_NAME;
+console.log('bucketttt',Bucket);
 
-aws.config.update({
-    accesskeyId: accesskeyId,
-    successkeyId: successkeyId,
-    bucketName: bucketName
+AWS.config.update({
+    
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+  Bucket: Bucket
+  // region: 'ap-south-1'
 })
+const s3 = new AWS.S3();
 
-const s3 = new aws.s3()
+const fileFilter = (request, file, cb) => {
+  //  console.log("requestQ in multer--",request);
+  
+
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+  } else {
+      console.log("in error");
+      cb('Invalid file type, only JPEG and PNG is allowed!');
+  }
+}
 
 const upload = multer({
+  
+  fileFilter,
+  storage: multerS3({
     s3: s3,
-    bucketName: bucketName,
-
+    bucket: Bucket,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    metadata: (request, file, cb) => {
+      console.log("request in multer--file-------", file);
+      cb(null, { fieldName: 'Profile' });
+    },
+    key: (request, file, cb) => {
+      // cb(null, file.originalname)
+      cb(null, file.originalname);    
+    }
+  })
 })
+module.exports = upload;
