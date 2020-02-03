088bab781e06cfe1ab5157a7db6c5fc1369c063c
Pravallika <skpravallikachowdary2@gmail.com>
2020-02-03 16:34:25 +0530

    [Pravallika]refactor.modified note controller for fundoo and pushed into remote

--- server/controller/noteController.js
+++ server/controller/noteController.js
@@ -1,11 +1,13 @@
-const Services = require('../services/noteServices')
-
+const Services = require('../services/noteServices')//note services
+const reminder = require('../helper/reminder')//reminder
 /**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
  * @param callback sends the data back or err
  * @return responses with a http response
 ***********************************************************/
+//exports addnote 
+//Add note by title and description
 exports.addNote = (request, res) => {
     request.checkBody('title', 'title is invalid').notEmpty()
     request.checkBody('description', 'description is invalid').notEmpty()
@@ -24,7 +26,6 @@ exports.addNote = (request, res) => {
                 response.data = data
                 res.status(200).send(response)
                 console.log(response);
-
             })
             .catch((err) => {
                 response.success = false;
@@ -32,15 +33,16 @@ exports.addNote = (request, res) => {
                 res.status(404).send(response)
 
             })
-
     }
 }
 /**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
  * @param callback sends the data back or err
  * @return responses with a http response
 ***********************************************************/
+//exports get all note
+//gets all notes of the user
 exports.getAllnote = (request, res) => {
     try {
         let response = {}
@@ -62,13 +64,13 @@ exports.getAllnote = (request, res) => {
     }
 }
 /**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
  * @param callback sends the data back or err
  * @return responses with a http response
 ***********************************************************/
 //exports delete note
-exports.deleteNote = (request, res) => {
+exports.isTrash = (request, res) => {
     try {
         request.checkBody('userId', 'userid is invalid').notEmpty()
         var error = request.validationErrors()
@@ -78,7 +80,7 @@ exports.deleteNote = (request, res) => {
             response.success = false;
             res.status(422).send(response)
         } else {
-            Services.deleteNote(request)
+            Services.isTrash(request)
                 .then((data) => {
                     response.success = true
                     response.data = data
@@ -96,8 +98,33 @@ exports.deleteNote = (request, res) => {
     }
 }
 /**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+//exports get delete note
+exports.unTrash = (request, res) => {
+    try {
+        let response = {}
+        Services.unTrash(request)
+            .then((data) => {
+                response.success = true
+                response.data = data
+                res.status(200).send(response)
+            })
+            .catch((err) => {
+                response.success = false;
+                response.err = err
+                res.status(400).send(response)
+            })
+    } catch (e) {
+        console.log(e)
+    }
+}
+/**********************************************************
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
  * @param callback sends the data back or err
  * @return responses with a http response
 ***********************************************************/
@@ -120,85 +147,10 @@ exports.updateNote = (request, res) => {
         console.log(e)
     }
 }
+
 /**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
- * @param callback sends the data back or err
- * @return responses with a http response
-***********************************************************/
-//exports get delete note
-exports.getDeleteNote = (request, res) => {
-    try {
-        let response = {}
-        Services.getDeleteNote(request)
-            .then((data) => {
-                response.success = true
-                response.data = data
-                res.status(200).send(response)
-            })
-            .catch((err) => {
-                response.success = false;
-                response.err = err
-                res.status(400).send(response)
-            })
-    } catch (e) {
-        console.log(e)
-    }
-}
-/**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
- * @param callback sends the data back or err
- * @return responses with a http response
-***********************************************************/
-//exports add collaborator
-exports.addCollaborator = (request, res) => {
-    try {
-        let response = {}
-        Services.addCollaborator(request)
-            .then((data) => {
-                response.success = true
-                response.data = data
-                res.status(200).send(response)
-            })
-            .catch((err) => {
-                response.success = false
-                response.err = err
-                res.status(400).send(response)
-            })
-    } catch (e) {
-        console.log(e)
-    }
-}
-/**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
- * @param callback sends the data back or err
- * @return responses with a http response
-***********************************************************/
-//exports get collaborator
-exports.getCollaborator = (request) => {
-    try {
-        request.checkBody('noteId', 'noteid is invalid').notEmpty()
-        let response = {}
-        Services.getCollaborator(request)
-            .then((data) => {
-                response.success = true
-                response.data = data
-                res.status(200).send(response)
-            })
-            .catch((err) => {
-                response.success = false
-                response.err = err
-                res.status(400).send(response)
-            })
-    } catch (e) {
-        console.log(e)
-    }
-}
-/**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
  * @param callback sends the data back or err
  * @return responses with a http response
 ***********************************************************/
@@ -208,8 +160,6 @@ exports.archive = (request, res) => {
         let response = {}
         Services.archive(request)
             .then((data) => {
-                console.log("dataa",data);
-                
                 response.success = true
                 response.data = data
                 res.status(200).send(response)
@@ -224,8 +174,32 @@ exports.archive = (request, res) => {
     }
 }
 /**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+//exports unarchive
+exports.unarchive=(request,res)=>{
+    try{
+        let response={}
+        Services.unarchive=(request)
+        .then((data)=>{
+            response.success=true
+            response.data=data
+            res.status(200).send(response)
+        }).catch((err)=>{
+            response.success=false
+            response.err=err
+            res.status(400).send(response)
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+/**********************************************************
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
  * @param callback sends the data back or err
  * @return responses with a http response
 ***********************************************************/
@@ -249,8 +223,8 @@ exports.getArchiveNote = (request, res) => {
     }
 }
 /**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
  * @param callback sends the data back or err
  * @return responses with a http response
 ***********************************************************/
@@ -260,6 +234,7 @@ exports.addReminder = (request, res) => {
         let response = {}
         Services.addReminder(request)
             .then((data) => {
+                reminder.reminderScheduler(data)
                 response.success = true
                 response.data = data
                 res.status(200).send(response)
@@ -274,8 +249,8 @@ exports.addReminder = (request, res) => {
     }
 }
 /**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
  * @param callback sends the data back or err
  * @return responses with a http response
 ***********************************************************/
@@ -300,28 +275,187 @@ exports.deleteReminder = (request, res) => {
     }
 }
 /**********************************************************
- *  @desc Gets the input from front end pass to model
- *  @param request request contains all the requested data
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
  * @param callback sends the data back or err
  * @return responses with a http response
 ***********************************************************/
 //exports create label
-exports.labelCreate=(request,res)=>{
-    try{
-        request.checkBody('labelName','noteName is invalid').notEmpty()
-        let response={}
-        Services.labelCreate(request)
-        .then((data)=>{
-            response.success=true
-            response.data=data
-            res.status(200).send(response)
-        })
-        .catch((err)=>{
-            response.success=false
-            response.err=err
-            res.status(400).send(response)
-        })
-    }catch(err){
+//creating a label
+exports.labelCreate = (request, res) => {
+    try {
+        request.checkBody('nameLabel', 'noteName is invalid').notEmpty()
+        let error = request.validationErrors()
+        let response = {}
+        if (error) {
+            response.success = false
+            response.error = error
+            res.status(422).send(response)
+        } else {
+            Services.labelCreate(request)
+                .then((data) => {
+                    response.success = true
+                    response.data = data
+                    res.status(200).send(response)
+                })
+                .catch((err) => {
+                    response.success = false
+                    response.err = err
+                    res.status(400).send(response)
+                })
+        }
+    } catch (err) {
         console.log(err)
+    }
+}
+/**********************************************************
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+//exports get label
+exports.getLabels = (request, res) => {
+    try {
+        let response = {}
+        Services.getLabels(request)
+            .then((data) => {
+                response.success = true
+                response.data = data
+                res.status(200).send(response)
+            })
+            .catch((err) => {
+                response.success = false
+                response.err = err
+                res.status(400).send(response)
+            })
+    } catch (e) {
+        console.log(e)
+    }
+}
+/**********************************************************
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+//exports update label
+exports.updateLabels = (request, res) => {
+    try {
+        let response = {}
+        Services.updateLabels(request)
+            .then((data) => {
+                response.success = true
+                response.data = data
+                res.status(200).send(response)
+            })
+            .catch((err) => {
+                response.success = false
+                response.err = err
+                res.status(400).send(response)
+            })
+    } catch (e) {
+        console.log(e)
+    }
+}
+/**********************************************************
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+//exports delete label
+exports.deleteLabels = (request, res) => {
+    try {
+        let response = {}
+        Services.deleteLabels(request)
+            .then((data) => {
+                response.success = true
+                response.data = data
+                res.status(200).send(response)
+            })
+            .catch((err) => {
+                response.success = false
+                response.err = err
+                res.status(400).send(response)
+            })
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+/**********************************************************
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+//exports add collaborator
+exports.addCollaborator = (request, res) => {
+    try {
+        let response = {}
+        Services.addCollaborator(request)
+            .then((data) => {
+                response.success = true
+                response.data = data
+                res.status(200).send(response)
+            })
+            .catch((err) => {
+                response.success = false
+                response.err = err
+                res.status(400).send(response)
+            })
+    } catch (e) {
+        console.log(e)
+    }
+}
+/**********************************************************
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+//exports delete collaborator
+exports.deleteCollaborator = (request, res) => {
+    try {
+        let response = {}
+        Services.deleteCollaborator(request)
+            .then((data) => {
+                response.success = true
+                response.data = data
+                res.status(200).send(response)
+            })
+            .catch((err) => {
+                response.success = false
+                response.err = err
+                res.status(400).send(response)
+            })
+    } catch (e) {
+        console.log(e)
+    }
+}
+/**********************************************************
+ * @desc Gets the input from front end pass to model
+ * @param request request contains all the requested data
+ * @param callback sends the data back or err
+ * @return responses with a http response
+***********************************************************/
+//exports get collaborator
+exports.getCollaborator = (request, res) => {
+    try {
+        let response = {}
+        Services.getCollaborator(request)
+            .then((data) => {
+                response.success = true
+                response.data = data
+                res.status(200).send(response)
+            })
+            .catch((err) => {
+                response.success = false
+                response.err = err
+                res.status(400).send(response)
+            })
+    } catch (e) {
+        console.log(e)
     }
 }
