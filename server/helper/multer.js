128f2bdb562c65e5205c9aaa4455ac13328df3a4
Pravallika <skpravallikachowdary2@gmail.com>
2020-02-03 16:35:24 +0530

    [Pravallika]refactor.modified multer for fundoo and pushed into remote

--- server/helper/multer.js
+++ server/helper/multer.js
@@ -1,22 +1,23 @@
 require('dotenv').config();
 const AWS = require('aws-sdk');
+//multer is a node js middleware for handling the multipart/form data for uploading the files
 const multer = require('multer');
+//multer s3 is used to store the data in aws
 const multerS3 = require('multer-s3');
 
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const Bucket = process.env.BUCKET_NAME;
 
-AWS.config.update({
-    
+AWS.config.update({   
   accessKeyId: accessKeyId,
   secretAccessKey: secretAccessKey,
   Bucket: Bucket
-  // region: 'ap-south-1'
 })
 const s3 = new AWS.S3();
-
+//fileFilter is used to control which files are to be accepted
 const fileFilter = (request, file, cb) => {
+  //mimetype of the file
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
       cb(null, true);
   } else {
@@ -31,14 +32,16 @@ const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: Bucket,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
+    contentType: multerS3.AUTO_CONTENT_TYPE,//the mimetype used to upload the file
     acl: 'public-read',
+    //metadata is a callback that accepts the request and file and returns a metadata object to be saved to s3
     metadata: (request, file, cb) => {
       console.log("request in multer--file-------", file);
       cb(null, { fieldName: 'Profile' });
     },
+    //key is used to represent the name of the file
     key: (request, file, cb) => {
-      // cb(null, file.originalname)
+    
       cb(null, file.originalname);    
     }
   })
